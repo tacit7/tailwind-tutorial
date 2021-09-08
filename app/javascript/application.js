ad6d2f6a08be3066199891cba165b8637facaa5b
uriel <uriel781@gmail.com>
2021-09-08 10:48:55 -0500

    update js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "@fortawesome/fontawesome-free/js/all";
+//https://stackoverflow.com/questions/74355756/how-to-add-font-awesome-to-rails-7-jsbundling
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ThemeSelectorController from "./theme_selector_controller"
+application.register("theme-selector", ThemeSelectorController)
