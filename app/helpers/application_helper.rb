9ba868056807397b8b3b26d8449dbdc2cc06b730
uriel <uriel781@gmail.com>
2021-09-09 13:43:53 -0500

    add some helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  def toggle_theme
+    cookies[:theme] == 'dark' ? 'light' : 'dark'
+  end
+
   def current_theme
     cookies[:theme] = 'light' if cookies[:theme].empty?
     cookies[:theme]
