6216674b82d9695dd178bb2908f7b4388abdcb45
uriel <uriel781@gmail.com>
2021-09-16 07:19:32 -0500

    update mytheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,29 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require('daisyui')]
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary":   "#a991f7",
+          "secondary": "#f6d860",
+          "accent":    "#37cdbe",
+          "neutral":   "#3d4451",
+          "base-100":  "#ffffff",
+          "info":      "#2094f3",
+          "success":   "#009485",
+          "warning":   "#ff9900",
+          "error":     "#ff5724",
+        },
+      },
+      "light",
+      "dark",
+    ],
+  },
 }
